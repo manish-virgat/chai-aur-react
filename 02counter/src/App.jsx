d3590d20d0a0171815cfa4c553e8edc374f6cfa5
Virgat <2269245@cognizant.com>
2024-05-29 12:19:04 +0530

    Uploaded Files

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    if (counter < 10) { setCounter(counter + 1); }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) { setCounter(counter - 1); }
+  }
+
+  return (
+    <>
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br /><br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
