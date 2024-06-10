1522582e543401325845c1d606247364ed0ffb8d
Virgat <2269245@cognizant.com>
2024-06-10 14:27:14 +0530

    adding files

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [color, setColor] = useState("white");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ backgroundColor: color, height: 500 }}>
+
+      <button onClick={() => setColor("olive")} style={{ marginTop: 250 }}>Olive</button>
+      <button onClick={() => setColor("black")} style={{ marginTop: 250 }}>Black</button>
+      <button onClick={() => setColor("red")} style={{ marginTop: 250 }}>Red</button>
+      <button onClick={() => setColor("blue")} style={{ marginTop: 250 }}>Blue</button>
+      <button onClick={() => setColor("green")} style={{ marginTop: 250 }}>Green</button>
+      <button onClick={() => setColor("pink")} style={{ marginTop: 250 }}>Pink</button>
+    </div>
   )
 }
 
